ed1c73843044d258d33e796b7e4cd6b5fe15c1f6
[tbedolf] <[tbedolf@gmail.com]>
2026-05-23 00:30:21 -0400

    Backend deployment prep

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,18 +1,22 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-
-import authRoutes from "./routes/auth.routes.js";
-import workspaceRoutes from "./routes/workspace.routes.js";
-import projectRoutes from "./routes/project.routes.js";
-import taskRoutes from "./routes/task.routes.js";
+import morgan from "morgan";
 
 dotenv.config();
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
+
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   res.json({
@@ -20,19 +24,8 @@ app.get("/", (req, res) => {
   });
 });
 
-app.use("/api/auth", authRoutes);
-app.use("/api/projects", projectRoutes);
-app.use("/api/tasks", taskRoutes);
-
-app.use(
-  "/api/workspaces",
-  workspaceRoutes
-);
-
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(
-    `Server running on port ${PORT}`
-  );
+  console.log(`Server running on port ${PORT}`);
 });
